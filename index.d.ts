7456ee04b18bc1ebab3432b14342729c30627ef3
Jason Walton <jwalton@benbria.ca>
2018-04-25 14:43:54 -0400

    Update typescript defs.

--- index.d.ts
+++ index.d.ts
@@ -10,8 +10,14 @@ interface PromiseBreakerInstance {
     make(asyncFn: Function): Function;
     break(options: MakeBreakOptions, promiseFn: Function): Function;
     break(promiseFn: Function): Function;
+
     addPromise(done: Function | undefined | null, asyncFn: Function): Promise<any> | null;
-    addCallback(done: Function | undefined | null, promiseFn: Function): Promise<any> | null;
+
+    addCallback<T>(
+        done: Function | undefined | null,
+        promise: (() => Promise<T>) | Promise<T>
+    ): Promise<T> | null;
+
     apply(fn: Function, thisArg?: any, args?: any[] | undefined) : Promise<any>;
     apply(fn: Function, thisArg: any, args: any[] | undefined, done: Function) : void;
     call(fn: Function, thisArg?: any, ...parameters: any[]) : Promise<any>;
